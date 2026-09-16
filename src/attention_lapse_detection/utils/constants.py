e9480c1eac18f75b93ff453c236534559014bdbb
ESMAT001 <esmat10.niazi@gmail.com>
2026-09-17 00:20:22 +0200

    Add basic paths functions

---
+++ src/attention_lapse_detection/utils/constants.py
@@ -0,0 +1,14 @@
+SEED = 42
+
+# Data
+LABELS_SPLITS = {
+    "Train": "TrainLabels.csv",
+    "Validation": "ValidationLabels.csv",
+    "Test": "TestLabels.csv",
+}
+
+FPS_OPTIONS = (10, 15, 30)
+WINDOW_SECONDS_OPTIONS = (5, 10)
+
+DEFAULT_WINDOW_SECONDS = 5
+DEFAULT_FPS = 10
